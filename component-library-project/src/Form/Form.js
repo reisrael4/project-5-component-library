c40be4903626a1f8c40552fbf7e56d0efe1028ea
RachelIsrael <racheleisrael4@gmail.com>
2020-02-24 11:27:06 -0500

    dropdowns and start on counter

--- component-library-project/src/Form/Form.js
+++ component-library-project/src/Form/Form.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Component } from 'react';
 import "./Form.css";
 
-let Form = props => {
+const Form = props => {
   let classList = "";
   let types = ["email", "dropdown", "counter", "code"];
   let placeholder = "";
@@ -10,26 +10,76 @@ let Form = props => {
     classList += ` form-${props.type}`;
   }
   if (props.medium) {
-    classList += ` email-medium`;
+    classList += ` ${props.type}-medium`;
   }
   if (props.large) {
-    classList += ` email-large`;
+    classList += ` ${props.type}-large`;
   }
-  if (!props.placeholder){
-        placeholder = `${props.label}`
-  } else{
-      placeholder = `${props.placeholder}`
+  if(props.shadow) {
+      classList += ` ${props.type}-shadow`;
   }
-
-  return (
-    <form>
-      <input
-        type="text"
-        placeholder={placeholder}
-        className={classList}
-      />
-    </form>
-  );
+  if (!props.placeholder) {
+    placeholder = `${props.label}`;
+  } else {
+    placeholder = `${props.placeholder}`;
+  }
+  if ((props.type === "email")) {
+    return (
+      <form>
+        <input type="text" placeholder={placeholder} className={classList} />
+      </form>
+    );
+  }
+  if((props.type === "dropdown")){
+      return (
+        <form>
+          <select className={classList}>
+            <option default>{placeholder}</option>
+            <option>A</option>
+            <option>B</option>
+            <option>C</option>
+            <option>D</option>
+          </select>
+        </form>
+      );
+  }
 };
 
-export default Form;
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+  increaseCount = () => {
+    let count = this.state.count + 1;
+    this.setState({
+      count: count
+    });
+  };
+  decreaseCount = () => {
+    let count = this.state.count - 1;
+    this.setState({
+      count: count
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Count: {this.state.count}</h1>
+          <button onClick={this.increaseCount}>+</button>
+          <button onClick={this.decreaseCount}>-</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default {
+    Counter, 
+    Form
+};
--- component-library-project/src/Form/Form.stories.js
+++ component-library-project/src/Form/Form.stories.js
@@ -3,6 +3,28 @@ import { storiesOf } from "@storybook/react";
 import Form from './Form'
 
 storiesOf("Form", module)
-  .add("Email", () => <Form label="Email" type="email" />)
-  .add("Medium Email", () => <Form label="Email" type="email" medium/>)
-  .add("Large Email", () => <Form label="Email" type='email' large/>)
+  .add("Email", () => <Form label="Email" type="email" input="text" />)
+  .add("Medium Email", () => (
+    <Form label="Email" type="email" input="text" medium />
+  ))
+  .add("Large Email", () => (
+    <Form label="Email" type="email" input="text" large />
+  ))
+  .add("Small Dropdown", () => (
+    <Form placeholder="Select" type="dropdown" input="select" />
+  ))
+  .add("Medium Dropdown", () => (
+    <Form placeholder="Select" type="dropdown" input="select" medium />
+  ))
+  .add("Large Dropdown", () => (
+    <Form placeholder="Select" type="dropdown" input="select" large />
+  ))
+  .add("Small Darker Dropdown", () => (
+    <Form placeholder="Select" type="dropdown" input="select" shadow/>
+  ))
+  .add("Medium Darker Dropdown", () => (
+    <Form placeholder="Select" type="dropdown" input="select" medium shadow/>
+  ))
+  .add("Large Darker Dropdown", () => (
+    <Form placeholder="Select" type="dropdown" input="select" large shadow/>
+  ));
