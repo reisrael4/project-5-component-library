85b70d808e39b58234cd9761b667555be6cf6d45
RachelIsrael <racheleisrael4@gmail.com>
2020-02-24 15:53:53 -0500

    storybook

---
+++ component-library-project/storybook-static/precache-manifest.5b5816595230100f33f3c72b6fd44a4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa822a8d6eb90edd6a43399e8c3fea41",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c61af1510d4951a49ab1.bundle.js"
+  },
+  {
+    "url": "runtime~main.c61af1510d4951a49ab1.bundle.js"
+  },
+  {
+    "revision": "1527dd533953297fd1ba",
+    "url": "static/css/main.0ba6a8e8.chunk.css"
+  },
+  {
+    "revision": "df57aa99bcd4e2c569120e802205b561",
+    "url": "static/media/cart_2.df57aa99.svg"
+  },
+  {
+    "revision": "0cce8b87858197e8f7e22e15399f6060",
+    "url": "static/media/hearts.0cce8b87.svg"
+  },
+  {
+    "url": "vendors~main.c61af1510d4951a49ab1.bundle.js"
+  },
+  {
+    "url": "vendors~main.c61af1510d4951a49ab1.bundle.js.LICENSE.txt"
+  }
+]);
