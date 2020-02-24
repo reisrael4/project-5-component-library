06f3718aa81877d0c8fa374bc7963e672f6aaf33
RachelIsrael <racheleisrael4@gmail.com>
2020-02-24 09:19:12 -0500

    email forms

--- component-library-project/src/Form/Form.js
+++ component-library-project/src/Form/Form.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
+import React from "react";
 import "./Form.css";
 
 let Form = props => {
-    let classList = "";
-    let types = ['email', 'dropdown', 'counter', 'code']
+  let classList = "";
+  let types = ["email", "dropdown", "counter", "code"];
 
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
-}
+  if (types.includes(props.type)) {
+    classList += ` form-${props.type}`;
+  }
+  if (props.medium) {
+    classList += ` email-medium`;
+  }
+  if (props.large) {
+    classList += ` email-large`;
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder={props.placeholder}
+        className={classList}
+      />
+    </form>
+  );
+};
 
 export default Form;
--- component-library-project/src/Form/Form.stories.js
+++ component-library-project/src/Form/Form.stories.js
@@ -1,4 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import Form from './Form'
 
-storiesOf('Form', module)
+storiesOf("Form", module)
+  .add("Email", () => <Form placeholder="Email" type="email" />)
+  .add("Medium Email", () => <Form placeholder="Email" type="email" medium/>)
+  .add("Large Email", () => <Form placeholder="Email" type='email' large/>)
